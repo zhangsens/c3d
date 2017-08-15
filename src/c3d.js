dc4d10c1aef4e7d964d92f388f9c88029b93a9d1
zhangss <zssqqzss@vip.qq.com>
2017-08-15 23:00:38 +0800

    点的坐标

--- src/c3d.js
+++ src/c3d.js
@@ -4,19 +4,17 @@ var c3d = function(ctx) {
     this.played = true;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
-    this._axis = new point(0, 0, 0);
+    this._axis = [0, 0];
     this.ax = e.clientX;
     this.ay = e.clientY;
-    this.t = new d3(100, 100, 100);
-    this._t = new d3(100, -100, 100);
-    this.axis([this.width / 2, 0, this.height / 2]);
+    this.axis([this.width / 2, this.height / 2]);
     this.canvas.addEventListener("mousedown", this.mousedown.bind(this), false);
     this.canvas.addEventListener("mousemove", this.mousemove.bind(this), false);
 }
 
 c3d.prototype = {
     data: function(data) {
-        this.data = [];
+        this.datas = [];
         var _data = [];
 
         for (let i in data) {
@@ -32,13 +30,13 @@ c3d.prototype = {
                     }
                 }
             }
-            this.data.push(__data);
+            this.datas.push(__data);
         }
-        return this.data;
+        return this.datas;
     },
 
     draw: function(data) {
-        var data = this.data;
+        var data = this.data(data);
         this.ctx.strokeStyle = "hsla(0,0%,0%,1)";
         for (let m in data) {
             for (let n in data[m]) {
@@ -61,86 +59,28 @@ c3d.prototype = {
         return pointArr;
     },
     axis: function(data) {
-        this.clearCanvas();
-        this.ctx.translate(-this._axis.x, -this._axis.y);
-        this._axis = new axis(data[0], data[1], data[2]);
-        this.ctx.translate(this._axis.x, this._axis.z);
-    },
-
-    //旋转
-    rotate: function() {
-        //横向距离
-        var rotateX = 20;
-        //纵向距离
-        var rotateY = 20;
-        //?
-        var rotateZ = 0;
-        //对X,y,Z处理
-
+        this._axis = data;
+        this.ctx.translate(this._axis[0], this._axis[1]);
     },
 
     clearCanvas: function() {
-        this.ctx.clearRect(-this.width / 2, -this.height / 2, this.width, this.height);
+        this.ctx.clearRect(-this._axis[0], -this._axis[1], this.width, this.height);
     },
 
     run: function() {
         if (this.played) {
             this.clearCanvas();
-            this.draw(this.data);
+            this.draw(this.datas);
             requestAnimationFrame(this.run.bind(this));
         }
-    },
-    test: function() {
-        this.ctx.beginPath();
-        this.ctx.strokeStyle = "hsla(10,100%,50%,1)";
-        var x = l(this.t._z, this.t._y) * Math.cos(this.t.nx) * (2000 + this.t.y) / 2000;
-        var y = l(this.t._x, this.t._y) * Math.cos(this.t.ny) * (2000 + this.t._y) / 2000;
-        console.log(this.t._z);
-        this.ctx.moveTo(0, 0);
-        this.ctx.lineTo(x, y);
-        this.ctx.stroke();
     },
     mousedown: function(e) {
         this.ax = e.clientX;
         this.ay = e.clientY;
     },
-    // mousemove: function(e) {
-    //     if (e.button == 0 && e.buttons == 1) {
-    //         var ay = e.clientY;
-    //         var ax = e.clientX;
-    //         var nx = Math.asin((this.ax - ax) / 200);
-    //         var ny = Math.asin((this.ay - ay) / 200);
-    //         // this.t._x = this.t.x * Math.cos(Y轴);
-    //         // this.t._y = this.t.y * Math.sin(Y轴) * Math.sin(X轴);
-    //         // this.t._z = this.t.z * Math.cos(X抽);
-    //         this.t.nx = this.t.ax + nx;
-    //         this.t.ny = this.t.ax + ny;
-    //         var sx = Math.pow(Math.sin(this.t.nx), 2);
-    //         var sy = Math.pow(Math.sin(this.t.ny), 2);
-    //         this.t._y = this.t.width * Math.sqrt(sx * sy / (sx + sy - sx * sy)) || 0;
-    //         this.t._x = this.t.y / (Math.round(Math.tan(this.t.ny) * 10000) / 10000);
-    //         this.t._z = this.t.y / (Math.round(Math.tan(this.t.nx) * 10000) / 10000);
-    //         console.log(this.t._x, this.t._y, this.t._z);
-
-    //         this.clearCanvas();
-    //         this.test();
-    //     }
-
-    // }
     mousemove: function(e) {
         if (e.button == 0 && e.buttons == 1) {
-            var ax = e.clientX;
-            var ay = e.clientY;
-            console.log(this.ax - ax);
-            console.log(this.ay - ay);
-            for (let i in this.data) {
-                // this.data[i][0]._x;
-                this.data[i][0]._y = this.data[i][0].y + this.ax - ax;
-                // this.data[i][0]._z;
-                this.data[i][0].canvasX = this.data[i][0]._x * (1000 + this.data[i][0]._y) / 1000;
-                this.data[i][0].canvasY = this.data[i][0]._z * (1000 + this.data[i][0]._y) / 1000;
-            }
-            this.clearCanvas();
+            //this.clearCanvas();
             this.draw();
         }
     }
--- src/point.js
+++ src/point.js
@@ -1,61 +1,32 @@
 function point(x, y, z) {
+
     this.x = x;
     this.y = y;
     this.z = z;
+
+    this.w = Math.sqrt(x * x + y * y + z * z);
     this.pi = Math.PI;
-    this.p = this.pi;
-    this._x = this.x;
-    this._y = this.y;
-    this._z = this.z;
-    this.angleX = 3 / 4 * this.pi;
-    this.angleY = 3 / 4 * this.pi;
-    this.canvasX = this._x * (1000 + this._y) / 1000; //this.x * Math.cos(this.angleX) - this.y * Math.sin(this.angleX);
-    this.canvasY = this._z * (1000 + this._y) / 1000; //this.z * Math.cos(this.angleY) - this.y * Math.sin(this.angleY);
-}
+    //与y轴所成角(面yx)
+    this.sinX = this.x / Math.sqrt(this.x * this.x + this.y * this.y);
+    this.cosX = this.y / Math.sqrt(this.x * this.x + this.y * this.y);
+    //与z轴所成角(面yz)
+    this.sinY = Math.sqrt(this.x * this.x + this.y * this.y) / this.w;
+    this.cosY = this.z / this.w;
 
-function axis(x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this.pi = Math.PI;
-    //转动X轴
-    this.angleX = 1 / 4 * this.pi;
-    //转动Y轴
-    this.angleY = 1 / 2 * this.pi;
-    //转动Z轴
-    this.angleZ = 1 / 4 * this.pi;
-}
+    if (!this.sinX && !this.cosX) {
+        this._x = 0;
+        this._y = 0;
+        this._z = this.w * this.cosY;
+    } else if (!this.sinY && !this.cosY) {
+        this._x = this.w * this.sinX;
+        this._y = 0;
+        this._z = 0;
+    } else {
+        this._x = this.w * this.sinY * this.sinX;
+        this._y = this.w * this.sinY * this.cosX;
+        this._z = this.w * this.cosY;
+    }
 
-function d3(x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this._x = this.x;
-    this._y = this.y;
-    this._z = this.z;
-
-    this.width = l(this.x, this.y, this.z);
-    this.zy = l(this.z, this.y);
-    this.xy = l(this.x, this.y);
-    this.ax = Math.asin(this.y / l(this.z, this.y));
-    this.ay = Math.asin(this.x / l(this.x, this.y));
-    //nx=0，z为最大值
-    this.nx = this.ax;
-    //ny=0，x为最大值
-    this.ny = this.ay;
-
-    //nx,ny
-    this.signX;
-    this.signY = Math.sin(this.nx) / Math.abs(Math.sin(this.nx)) * Math.sin(this.ny) / Math.abs(Math.sin(this.ny));
-    this.signZ;
-
-    var sx = Math.pow(Math.sin(this.nx), 2);
-    var sy = Math.pow(Math.sin(this.ny), 2);
-    this._y = this.width * Math.sqrt(sx * sy / (sx + sy - sx * sy));
-    this._x = this.y / (Math.round(Math.tan(this.ny) * 10000) / 10000);
-    this._z = this.y / (Math.round(Math.tan(this.nx) * 10000) / 10000);
-
-    this.canvasX = l(this._z, this._y) * Math.cos(this.nx) * (2000 + this.y) / 2000;
-    this.canvasY = l(this._x, this._y) * Math.cos(this.ny) * (2000 + this.y) / 2000;
-
+    this.canvasX = this._x * (1000 + this._y) / 1000;
+    this.canvasY = -this._z * (1000 + this._y) / 1000;
 }
